0613149525a53319fbea2d8d71fefd9aa1562df9
ArweleD <91463609+ArweleD@users.noreply.github.com>
2021-10-06 10:15:32 +0100

    Add files via upload

---
+++ Motorbike extension task.py
@@ -0,0 +1,8 @@
+def motorbike_Cost(a):
+    while a > 1000:
+        
+     a *= 0.9
+    print(a)
+    
+
+print(motorbike_Cost(4000))     
---
+++ Writing into data task.py
@@ -0,0 +1,5 @@
+my_file = open("Numbers.txt", "w")
+
+my_file.write("3,45,83,21")
+
+my_file.close()
